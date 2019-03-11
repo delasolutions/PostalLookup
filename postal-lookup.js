8fe0b537d7fa1d4b0bd18390c9deacdb0766eba3
Juan Cruz <juan@cherre.com>
2019-03-11 16:34:04 -0400

    finalized application and removed private and secret keys

--- postal-lookup.js
+++ postal-lookup.js
@@ -1,22 +1,24 @@
 // Typesscript
 // postal-lookup.ts
 var fs = require('fs');
-var data = fs.readFileSync("Zip_Code_Lookup_Table.csv", "utf-8");
-var results = data.split("\n");
-for (var _i = 0, results_1 = results; _i < results_1.length; _i++) {
-    var city = results_1[_i];
-    console.log(city);
+var file = fs.readFileSync("Zip_Code_Lookup_Table.csv", "utf-8");
+var rows = file.split("\n");
+for (var _i = 0, rows_1 = rows; _i < rows_1.length; _i++) {
+    var row = rows_1[_i];
+    var columns = row.split(",");
+    var zip = columns[0].toString().trim();
+    // console.log(zip);
+    var OAuth = require('oauth');
+    var header = {
+        "X-Yahoo-App-Id": "Oteqhx34"
+    };
+    var request = new OAuth.OAuth(null, null, 'private key', 'secret key', '1.0', null, 'HMAC-SHA1', null, header);
+    request.get('https://weather-ydn-yql.media.yahoo.com/forecastrss?location=' + zip + '&format=json', null, null, function (err, data, result) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(data);
+        }
+    });
 }
-var OAuth = require('oauth');
-var header = {
-    "X-Yahoo-App-Id": "Oteqhx34"
-};
-var request = new OAuth.OAuth(null, null, 'dj0yJmk9WUZrMGxsZVJqaDVvJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTdj', '409839ee9ae96db860b955fb53cf4c5a631ede58', '1.0', null, 'HMAC-SHA1', null, header);
-request.get('https://weather-ydn-yql.media.yahoo.com/forecastrss?location=sunnyvale,ca&format=json', null, null, function (err, data, result) {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log(data);
-    }
-});
--- postal-lookup.ts
+++ postal-lookup.ts
@@ -3,37 +3,38 @@
 
 const fs = require('fs')
 
-const data = fs.readFileSync("Zip_Code_Lookup_Table.csv", "utf-8");
-let results:string[] = data.split("\n");
+const file = fs.readFileSync("Zip_Code_Lookup_Table.csv", "utf-8");
+let rows:string[] = file.split("\n");
 
-for (var city of results){
-    console.log(city);
+for (var row of rows){
+    let columns:string[] = row.split(",");
+    var zip = columns[0].toString().trim()
+    // console.log(zip);
+    var OAuth = require('oauth');
+    var header = {
+      "X-Yahoo-App-Id": "Oteqhx34"
+};
+    var request = new OAuth.OAuth(
+      null,
+      null,
+      'private key',
+      'secret key',
+      '1.0',
+      null,
+      'HMAC-SHA1',
+      null,
+      header
+);
+    request.get(
+      'https://weather-ydn-yql.media.yahoo.com/forecastrss?location=' + zip + '&format=json',
+      null,
+      null,
+      function (err, data, result) {
+          if (err) {
+              console.log(err);
+          } else {
+              console.log(data)
+          }
+      }
+  );
 }
-
-// var OAuth = require('oauth');
-// var header = {
-//     "X-Yahoo-App-Id": "Oteqhx34"
-// };
-// var request = new OAuth.OAuth(
-//     null,
-//     null,
-//     'private key',
-//     'secreat key',
-//     '1.0',
-//     null,
-//     'HMAC-SHA1',
-//     null,
-//     header
-// );
-// request.get(
-//     'https://weather-ydn-yql.media.yahoo.com/forecastrss?location=sunnyvale,ca&format=json',
-//     null,
-//     null,
-//     function (err, data, result) {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             console.log(data)
-//         }
-//     }
-// );
